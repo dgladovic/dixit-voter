ac8099e27825e84ec589ae8eda7f555eb748ff03
Dimitrije Gladovic <rdomiran@gmail.com>
2023-09-20 15:12:19 +0200

    updated storyteller message UI and look

--- dixit-voter/src/StorytellerMessage.js
+++ dixit-voter/src/StorytellerMessage.js
@@ -5,7 +5,6 @@ function StorytellerMessage({ key, storyteller, player }) {
   const [isVisible, setIsVisible] = useState(true);
 
   useEffect(() => {
-    console.log('CHANGE',storyteller,player);
     const timer = setTimeout(() => {
       setIsVisible(false);
     }, 4000);
@@ -23,12 +22,23 @@ function StorytellerMessage({ key, storyteller, player }) {
   return (
     <div className="storyteller-message">
       {player.name === storyteller.name ? (
-        <p>Tell a story!</p>
-      ) : (
-        <p>{storyteller.name} is telling a story</p>
+        <div>
+          <span style={{ display: 'inline-block', margin: 'auto' }}>Tell a</span>
+          <span id="capitalS" style={{ fontFamily: 'Dilana', fontSize: '36px', textTransform: 'capitalize', marginLeft: '6px' }}>S</span>
+          <span style={{ display: 'inline-block', margin: 'auto', color: 'rgb(228,213,116)', fontFamily: 'Dilana', fontSize: '32px', }}>tory</span>
+        </div>
+      ) : storyteller.name && (
+        <div>
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-end' }}>
+            <span id="capitalS" style={{ fontFamily: 'Dilana', fontSize: '36px', textTransform: 'capitalize' }}>{storyteller.name[0]}</span>
+            <span id="capitalS-rest" style={{ fontFamily: 'Dilana', fontSize: '28px' }}>{storyteller.name.slice(1)}</span>
+          </div>
+          <span style={{ display: 'inline-block', margin: 'auto' }}>Is telling a story</span>
+        </div>
       )}
     </div>
   );
 }
 
 export default StorytellerMessage;
+
