eb7875fae0f1e533f737bd54855a0494ce3ebc9b
Dimitrije Gladovic <dimitrije.gladovic@asseco-see.rs>
2023-09-21 13:07:59 +0200

    added .env files

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import DixitCard from './DixitCard';
 import { Grid, Button, Box } from '@mui/material';
 import PlayerBanner from './PlayerBanner';
 
-const socket = io('http://localhost:3000'); // Replace with your server's URL
+const socket = io(process.env.REACT_APP_API_URL); // Replace with your server's URL
 
 function App() {
   const [rooms, setRooms] = useState([]);
