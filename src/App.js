fe85c9729be7076a6081fb08f080d42b9af16739
Dimitrije Gladovic <rdomiran@gmail.com>
2023-09-22 01:39:07 +0200

    restore player data from session

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ function App() {
   const [voteStatus, setVoteStatus] = useState([]);
   const [ownershipStatus, setOwnershipStatus] = useState([]);
   const [connected,setConnected] = useState(false);
+  const [saveSession,setSaveSession] = useState(true);
 
 
 
@@ -58,34 +59,68 @@ function App() {
   useEffect(() => {
     const sessionID = localStorage.getItem("sessionID");
     if(sessionID){
-      console.log('hej')
+      console.log('hej',sessionID);
       // setFirstLog(false);
       socket.auth = {sessionID};
       socket.connect();
-    //   setConnected(true);
-      // socket.emit('joinRoom', JSON.stringify());
-          // data
-    // playerName: name,
-    // roomName: room,
-    // color: selectedColor,
+      // kada se konektuje na socket sa vec sacuvanim session id radi se
+      // na serveru socket.emit(session)
+      // i onda se pokrece useEffect ispod ovoga
+
     }
   }, []);
 
   useEffect(() => {
     // Listen for session update
     socket.on('session', ({ sessionID, userID, userScore, name, roomName }) => {
+      console.log(socket,'socketo-sesija');
       socket.auth = {sessionID};
-      localStorage.setItem("sessionID",sessionID);
       socket.userID = userID;
       socket.userScore = userScore;
       socket.name = name;
       socket.roomName = roomName;
+      console.log(socket,'socketo-sesija-nakondodele');
+      // sa servera preko session kanala dobija podatke o sesiji
+      // ti podaci se stavljaju na socket konekciju
+      // i zatim se proverava, ukoliko socket ima roomName, znaci da je vec bio u sobi 
       if(roomName){
         console.log(socket,'socketo');
         setFirstLog(false);
+        setSaveSession(false);
+        // nema potrebe da se bira soba, direktno ga vraca u igru
+        let reconnectionObj = {
+          playerName: socket.name,
+          roomName: socket.roomName,
+          reconnect: 'yes'
+        }
+        socket.emit('joinRoom', JSON.stringify(reconnectionObj));
+        setCurrentRoom(socket.roomName);
+        const Player = {
+          id: '',
+          name: socket.name,
+          score: socket.userScore,
+          // color: data.color
+        }
+        setPlayer(Player);
       }
+    });
+  }, []);
 
-      // sve se vraca na socket, kako bi on sadrzao sve sto je bitno za sesiju
+  useEffect(() => {
+    // Listen for session update
+    socket.on('session', ({ sessionID, userID, userScore, name, roomName }) => {
+      if(saveSession){
+        socket.auth = {sessionID};
+        localStorage.setItem("sessionID",sessionID);
+        socket.userID = userID;
+        socket.userScore = userScore;
+        socket.name = name;
+        socket.roomName = roomName;
+        // sa servera preko session kanala dobija podatke o sesiji
+        // ti podaci se stavljaju na socket konekciju
+        // i zatim se proverava, ukoliko socket ima roomName, znaci da je vec bio u sobi 
+      }
+      setSaveSession(false);
     });
   }, []);
 
@@ -93,6 +128,7 @@ function App() {
     // Listen for cards to render
     socket.on('cardList', (message) => {
       let newMess = JSON.parse(message);
+      console.log(newMess,'socketo-cards');
       setButtons(newMess);
     });
   }, []);
