7d514072909f486493121b72a8328a9e66314ad7
Dimitrije Gladovic <dimitrije.gladovic@asseco-see.rs>
2023-10-05 20:54:46 +0200

    updated linting for network listeners

--- src/NetworkHandlers/StoryTellerListener.js
+++ src/NetworkHandlers/StoryTellerListener.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import {useEffect} from 'react';
 
 const StoryTellerListener = ({socket, setStoryteller, setShowStartGame, setCheckStoryTeller, storyTeller, player}) =>{
