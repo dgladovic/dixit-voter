fb25e0c1df6830657d0eeedc9ff5eb57d6f8a132
Dimitrije Gladovic <dimitrije.gladovic@asseco-see.rs>
2023-10-10 14:56:50 +0200

    added Transition Message
    This message tells user that the Ownership phase is set to begin

---
+++ src/TransitionMessage.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import './StorytellerMessage.css'; // Import the CSS file
+
+function TransitionMessage({ key }) {
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(false);
+    }, 1500);
+
+    return () => {
+      clearTimeout(timer);
+      setIsVisible(true);
+    }
+  }, [key]);
+
+  if (!isVisible) {
+    return null;
+  }
+
+  return (
+    <div className="storyteller-message">
+        <div>
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-end' }}>
+            <span id="capitalS" style={{ fontFamily: 'Dilana', fontSize: '36px', textTransform: 'capitalize' }}>O</span>
+            <span id="capitalS-rest" style={{ fontFamily: 'Dilana', fontSize: '28px' }}>wnership</span>
+          </div>
+          <span style={{ display: 'inline-block', margin: 'auto' }}>Select the position of your card</span>
+        </div>
+    </div>
+  );
+}
+
+export default TransitionMessage;
+
