5a38d8d02e56ffefd90de18e955168f3b627069a
Dimitrije Gladovic <dimitrije.gladovic@asseco-see.rs>
2024-02-26 12:20:09 +0100

    playerHeader changed, added Settings Button

--- src/PlayerBanner.js
+++ src/PlayerBanner.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Card, CardContent, Typography, Grid } from '@mui/material';
 import './PlayerBanner.css';
-import {Star} from "@mui/icons-material";
+import {Star, Settings} from "@mui/icons-material";
 
 function PlayerBanner({ player }) {
   const [players, setPlayers] = useState([]);
@@ -16,18 +16,26 @@ function PlayerBanner({ player }) {
 
           <div class='banner-container'>
 
-            <div className="banner">
-              <span id="capital" style={{ fontFamily: 'Dilana', fontSize: '38px', textTransform:'capitalize' }}>{player.name[0]}</span>
-              <span id="capital-rest" style={{ fontFamily: 'Dilana', fontSize: '28px'}}>{player.name.slice(1)}</span>
-            </div>
-            
-            <div style={{display:'flex', alignItems:'center', marginTop:'20px', marginRight:'16px'}}>
+            <div style={{display:'flex', alignItems:'center', marginLeft:'16px', width:'42px'}}>
               <div style={{display:'inline-block', marginRight:'-16px', marginTop:'6px'}}>
                 <Star style={{fontSize:'28px', color:'black',position:'relative', top:'0px',left:'0px'}}/>
                 <Star style={{fontSize:'20px', color: player.color, position:'relative', top:'-4px',left:'-24px', zIndex:50}}/>
               </div>
               <span style={{fontWeight:'bold'}}>{player.score}</span>
             </div>
+
+            <div className="banner">
+              <span id="capital" style={{ fontFamily: 'Dilana', fontSize: '38px', textTransform:'capitalize' }}>{player.name[0]}</span>
+              <span id="capital-rest" style={{ fontFamily: 'Dilana', fontSize: '28px'}}>{player.name.slice(1)}</span>
+            </div>
+
+            <div style={{display:'flex', alignItems:'center', marginRight:'16px', width:'42px', justifyContent:'flex-end'}}>
+              {/* <div style={{display:'inline-block', marginRight:'-16px', marginTop:'6px'}}> */}
+                <Settings style={{fontSize:'28px', color:'black', position:'relative', top:'2px'}}/>
+              {/* </div> */}
+            </div>
+            
+
           </div>
   );
 }
