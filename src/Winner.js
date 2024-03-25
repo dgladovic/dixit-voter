4878bfca781f64bc0f92a75311c32b7a1c2ca59c
Dimitrije Gladovic <dimitrije.gladovic@asseco-see.rs>
2024-03-25 15:18:23 +0100

    Update Winner.js

--- src/Winner.js
+++ src/Winner.js
@@ -5,12 +5,13 @@ import UserInfoRoomSelection from './UserInfoRoomSelection';
 import logo from './dixit-logo.png';
 import './LandingPage.css';
 import { StarBorder } from '@mui/icons-material';
+import { Button } from '@mui/material';
 
 
 const Winner = ({
-    winner
+  winner
 }) => {
-    
+
   //   const [animationPhase, setAnimationPhase] = useState('appear');
 
   // useEffect(() => {
@@ -21,38 +22,56 @@ const Winner = ({
   // }, []);
 
 
-    return (
-        <div style={{display: 'flex', flexDirection:'column', margin:'auto', alignItems:'center',width:'100%'}}>
-          {/* className={`animated-div ${animationPhase}`}
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', margin: 'auto', alignItems: 'center', width: '100%' }}>
+      {/* className={`animated-div ${animationPhase}`}
           onAnimationEnd={() => {
             if (animationPhase === 'appear') {
               // Wait for the 'appear' animation to complete before triggering 'moveUp'
               setAnimationPhase('moveUp');
             }
           }} */}
-          <h3>We have a WINNER!</h3>
-          <h1 style={{textTransform:'capitalize'}}>{winner.name}</h1>
-          <StarBorder/>
-          <h3>you are the best!</h3>
-        <div style={{ display: 'flex', marginTop:'40px',width:'90%',justifyContent:'space-around' }}>
-          <div style={{ border:'solid 2px black', height: '180px', width: '100px', flexDirection: 'column', display: 'flex', alignItems: 'center' }}>
-            <h3>Dreamer</h3>
-            <StarBorder/>
-            <h3>Milica</h3>
-          </div>
-          <div style={{ border:'solid 2px black', height: '180px', width: '100px', flexDirection: 'column', display: 'flex', alignItems: 'center' }}>
-            <h3>Talker</h3>
-            <StarBorder/>
-            <h3>Ana</h3>
-          </div>
-          <div style={{ border:'solid 2px black', height: '180px', width: '100px', flexDirection: 'column', display: 'flex', alignItems: 'center' }}>
-            <h3>Guesser</h3>
-            <StarBorder/>
-            <h3>Isidora</h3>
-          </div>
+      <h3>We have a WINNER!</h3>
+      <h1 style={{ textTransform: 'capitalize' }}>{winner.name}</h1>
+      <StarBorder />
+      <h3>you are the best!</h3>
+      <div style={{ display: 'flex', marginTop: '40px', width: '90%', justifyContent: 'space-around' }}>
+        <div style={{ border: 'solid 2px black', height: '180px', width: '100px', flexDirection: 'column', display: 'flex', alignItems: 'center' }}>
+          <h3>Dreamer</h3>
+          <StarBorder />
+          <h3>Milica</h3>
         </div>
+        <div style={{ border: 'solid 2px black', height: '180px', width: '100px', flexDirection: 'column', display: 'flex', alignItems: 'center' }}>
+          <h3>Talker</h3>
+          <StarBorder />
+          <h3>Ana</h3>
         </div>
-    );
+        <div style={{ border: 'solid 2px black', height: '180px', width: '100px', flexDirection: 'column', display: 'flex', alignItems: 'center' }}>
+          <h3>Guesser</h3>
+          <StarBorder />
+          <h3>Isidora</h3>
+        </div>
+      </div>
+      <div style={{display:'flex', width:'90%',margin:'auto', marginTop:'16px'}}>
+      <Button
+          style={{ width: '48%', color: 'white', backgroundColor: '#06b306', height: '50px', margin:'auto', marginTop: '0px' }}
+          variant="contained"
+          // color="primary"
+          // onClick={handleConfirmClick}
+        >
+          Play again
+        </Button>
+        <Button
+          style={{ width: '48%', color: 'white', backgroundColor: 'black', height: '50px', margin:'auto', marginTop: '0px' }}
+          variant="contained"
+          // color="primary"
+          // onClick={handleConfirmClick}
+        >
+          Change Mode
+        </Button>
+        </div>
+    </div>
+  );
 }
 
 export default Winner;
